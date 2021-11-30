03356267b14b36d5e837fc778d72af848a83055e
shopnb <575994260@qq.com>
2021-11-30 14:10:08 +0800

    修改宽度

--- src/locales/panels.ts
+++ src/locales/panels.ts
@@ -3,6 +3,7 @@ export default {
     panels: {
       Component: '组件',
       OutlinedTree: '树形大纲',
+      ResourceManages: '资源管理',
       PropertySettings: '属性配置',
       History: '历史记录',
     },
@@ -11,6 +12,7 @@ export default {
     panels: {
       Component: 'Component',
       OutlinedTree: 'Outlined Tree',
+      ResourceManages: 'Resource Manages',
       PropertySettings: 'Property Settings',
       History: 'History',
     },
